21231c3d8208b9f74d480b6a9e133e11ce6b07ca
smitha <smithajojo@gmail.com>
2019-01-23 08:37:51 +0000

    SetUp Test

---
+++ WebDriverTesting/TestStartup.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebDriverTesting
+{
+    [TestFixture]
+    public class TestStartup
+    {
+        [TestCase]
+        public void Execute()
+        {
+            IWebDriver webDriver = new ChromeDriver();
+            webDriver.Url = @"http://google.com";
+            Thread.Sleep(3000);
+            webDriver.Close();
+        }
+    }
+}
---
+++ WebDriverTesting/TestSuiteDemo.cs
@@ -0,0 +1,98 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace WebDriverTesting
+{
+    [TestFixture]
+    public class TestSuiteDemo
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new ChromeDriver();
+            baseURL = "https://www.katalon.com/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        
+        public void ManoramaPageTest()
+        {
+            driver.Navigate().GoToUrl("https://www.manoramaonline.com/home.html");
+            driver.FindElement(By.LinkText("HOMESTYLE")).Click();
+            Thread.Sleep(2000);
+            driver.FindElement(By.XPath("(.//*[normalize-space(text()) and normalize-space(.)='/'])[1]/following::span[1]")).Click();
+            Thread.Sleep(2000);
+            
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
